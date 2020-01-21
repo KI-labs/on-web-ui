002fbfb8368b709c83d156bd3fd0c8eda9488c22
danieltolmon <danieltoledomonfort@Daniels-MacBook-Pro.local>
2020-01-21 13:44:43 +0000

    change path tasks from specs

--- src/app/management-center/tasks/tasks.component.spec.ts
+++ src/app/management-center/tasks/tasks.component.spec.ts
@@ -1,6 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { CustomTaskComponent } from './custom-task.component';
+import { CustomTaskComponent } from './tasks.component';
 
 describe('CustomTaskComponent', () => {
   let component: CustomTaskComponent;
